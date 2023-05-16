e9e6dc8abec65fb5488bd5e7d101ebf7ed61692b
Patryk Ostern <patryk.ostern@justwatch.com>
2023-05-16 18:33:16 +0200

    change naming

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -4,34 +4,40 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     }
   });
 
-  function modifyWords(paragraphs) {
+  function modifyWords(element) {
+    let newElement = '';
 
-    for (let i = 0; i < paragraphs.length; i++) {
-      const paragraph = paragraphs[i];
-      const text = paragraph.textContent.trim().split(" ");
-      const modifiedWords = [];
+    element.forEach(function (item) {
+      // Change of word to be bold as accouring to bionic reading method
+      let boldWord = '';
+    
+      if (item.length == 1) {
+        boldWord += `<b>${item}</b>`;
+      }
+      else if (item.length <= 3 && item.length != 1) {
+        boldWord += `<b>${item.substr(0, 1)}</b><span style="font-weight: 400;">${item.substr(1)}</span>`;
+      }
+      else if (item.length > 3 && (item.length % 2 == 0)) {
+        boldWord += `<b>${item.substr(0, item.length / 2)}</b><span style="font-weight: 400;">${item.substr(item.length / 2)}</span>`;
+      } else {
+        boldWord += `<b>${item.substr(0, item.length / 2 + 1)}</b><span style="font-weight: 400;">${item.substr(item.length / 2 + 1)}</span>`;
+      }
 
-      let newParagraph = '';
-      text.forEach(function (item) {
-        // Change of word to be bold as accouring to bionic reading method
-        let boldWord = '';
-      
-        if (item.length == 1) {
-          boldWord += `<b>${item}</b>`;
-        }
-        else if (item.length <= 3 && item.length != 1) {
-          boldWord += `<b>${item.substr(0, 1)}</b><span style="font-weight: 400;">${item.substr(1)}</span>`;
-        }
-        else if (item.length > 3 && (item.length % 2 == 0)) {
-          boldWord += `<b>${item.substr(0, item.length / 2)}</b><span style="font-weight: 400;">${item.substr(item.length / 2)}</span>`;
-        } else {
-          boldWord += `<b>${item.substr(0, item.length / 2 + 1)}</b><span style="font-weight: 400;">${item.substr(item.length / 2 + 1)}</span>`;
-        }
+      newElement += boldWord + ' ';
+    });
 
-        newParagraph += boldWord + ' ';
+    return newElement
+  }
 
-      });
-      paragraph.innerHTML = newParagraph;
+  function modifyElementsContent(elements) {
+
+    for (var i = elements.length; i--;) {
+      const element = elements[i];
+      const text = element.textContent.trim().split(" ");
+
+      newElementContent = modifyWords(element)
+
+      element.innerHTML = newElementContent;
       
     }
   }
@@ -39,8 +45,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   function modifyDOM() {
     
     const paragraphs = document.getElementsByTagName('p');
-
-    modifyWords(paragraphs);
+    modifyElementsContent(paragraphs);
 
 }
 
