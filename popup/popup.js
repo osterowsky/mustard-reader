61e31ddd2e3a62133c881eb821f5792b33d9f845
Patryk Ostern <patryk.ostern@justwatch.com>
2023-05-22 21:54:12 +0200

    change toggle button

--- popup/popup.js
+++ popup/popup.js
@@ -1,14 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.getElementById('toggleButton');
-    const toggleText = document.getElementById('toggleText');
-
-    toggleText.textContent = 'Turn us on';
   
     toggleButton.addEventListener('change', function() {
       chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         const activeTab = tabs[0];
         chrome.tabs.sendMessage(activeTab.id, { action: "toggleTurboVue" });
-        toggleText.textContent = toggleButton.checked ? 'Turn us off' : 'Turn us on';
       });
     });
   });
